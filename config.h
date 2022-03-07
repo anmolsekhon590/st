1e94bc7ed05e1d86de3ac98febd31228a90311fb
anmol <anmol@dellpc.ht.home>
2022-03-06 22:14:27 -0500

    added scrollback

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "RobotoMono-Regular:pixelsize=18:antialias=true:autohint=true";
+static char *font = "RobotoMono-Regular:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 float alpha = 0.8;
